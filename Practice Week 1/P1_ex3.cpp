742a874617622bef96d1da34ea06941616479266
sovanraksa-lat <lat.raksa06@gmail.com>
2025-08-07 12:00:46 +0700

    P1ex3 and P2ex2

---
+++ Practice Week 1/P1_ex3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int num;
+    cout <<"Enter a number: ";
+    cin >> num;
+    if (num%3 == 0){
+        cout << "The number is divisible by 3";
+    }else{
+        cout << "The number is not divisible by 3";
+    }
+    return 0;
+}
