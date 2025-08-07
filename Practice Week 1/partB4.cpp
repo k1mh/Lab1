4605e0aa668b25b19141280c8ca156c07b55b09e
yann laiey <yannseaklay2412@gmail.com>
2025-08-07 14:06:43 +0700

    Update partB4.cpp

--- Practice Week 1/partB4.cpp
+++ Practice Week 1/partB4.cpp
@@ -3,13 +3,24 @@ int main(){
 
 
     int num;
+    bool prime=true;
     std::cout<<"Enter  a  number :";
     std::cin>>num;
-    for(int i=2;i<=num/2;i++){
-    if(num % i!=0){
-        std::cout<<"It's not prime  number.";
+    if(num<=1){
+        prime=false;
     }else{
-        std::cout<<"It's prime number.";
-    }}
+        
+    for(int i=2;i<=num/2;i++){
+    if(num % i==0){
+       prime=false;
+       break;}
+    }
+    }
+    
+if(prime){
+     std::cout<<"It's prime  number.";
+}else{
+    std::cout<<"It's  not prime  number.";
+}
     return 0;
 }
